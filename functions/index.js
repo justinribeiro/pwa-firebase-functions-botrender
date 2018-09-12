36fd14648c210e74b31f3321b18e9073b66567db
Justin Ribeiro <justin@justinribeiro.com>
2018-09-12 09:11:24 -0700

    Update deps

--- functions/index.js
+++ functions/index.js
@@ -5,10 +5,12 @@ const url = require('url');
 const app = express();
 
 const pwaShell = require('./pwashell');
+const FIREBASE_CONFIG = JSON.parse(process.env.FIREBASE_CONFIG);
 
 /**
  * generateUrl() - Piece together request parts to form FQDN URL
  * @param {Object} request
+ * @return {String} url
  */
 const generateUrl = (request) => {
   // Why do we use functions.config().site.domain instead of the domain from
@@ -16,7 +18,7 @@ const generateUrl = (request) => {
   // cloudfunctions.net)
   return url.format({
     protocol: request.protocol,
-    host: functions.config().site.domain,
+    host: FIREBASE_CONFIG.site.domain,
     pathname: request.originalUrl,
   });
 };
@@ -24,13 +26,14 @@ const generateUrl = (request) => {
 /**
  * checkForBots() - regex that UserAgent, find me a linkbot
  * @param {String} userAgent
+ * @return {Boolean}
  */
 const checkForBots = (userAgent) => {
-    // These are link bots only!
-    // DO NOT ADD GOOGLEBOT.
-    // If you add Googlebot to this, you will not have a good day.
-    // This is a mix of Sam Li's list (https://github.com/webcomponents/webcomponents.org/blob/696eb6d6f1fe955db395e96d97c3d1dfe0a02b26/client/bot-filter.py#L9)
-    // and my list (https://github.com/justinribeiro/blog-pwa/blob/a7174657f3e910cacf2f089c012d40bec719293e/appengine/main.py#L28)
+    // These are link bots only! DO NOT ADD GOOGLEBOT. If you add Googlebot to
+    // this, you will not have a good day. This is a mix of Sam Li's list
+    // (https://github.com/webcomponents/webcomponents.org/blob/696eb6d6f1fe955db395e96d97c3d1dfe0a02b26/client/bot-filter.py#L9)
+    // and my list
+    // (https://github.com/justinribeiro/blog-pwa/blob/a7174657f3e910cacf2f089c012d40bec719293e/appengine/main.py#L28)
     const botList = 'baiduspider|facebookexternalhit|twitterbot|rogerbot|linkedinbot|embedly|quora\ link\ preview|showyoubot|outbrain|pinterest|slackbot|vkShare|W3C_Validator|slackbot|facebot|developers\.google\.com\/\+\/web\/snippet\/'.toLowerCase();
 
     // FIND THE BOT AMONG THE USSSERRRS
@@ -42,9 +45,11 @@ const checkForBots = (userAgent) => {
 };
 
 // This WILL NOT run for index.html because Exact-match static content is before
-// configured rewrites (see "Hosting Priorities" https://firebase.google.com/docs/hosting/url-redirects-rewrites)
+// configured rewrites (see "Hosting Priorities"
+// https://firebase.google.com/docs/hosting/url-redirects-rewrites)
 //
-// The trick is on L66, pwaShell(): You must update that file! Open for explainer.
+// The trick is on L66, pwaShell(): You must update that file! Open for
+// explainer.
 app.get('*', (req, res) => {
   // What say you bot tester?
   const botResult = checkForBots(req.headers['user-agent']);
@@ -53,9 +58,10 @@ app.get('*', (req, res) => {
     // Get me the url all nice
     const targetUrl = generateUrl(req);
 
-    // Did you read the README? You should have set functions.config().rendertron.server
-    // to where ever you deployed https://github.com/GoogleChrome/rendertron on AppEngine
-    fetch(`${functions.config().rendertron.server}/render/${targetUrl}`)
+    // Did you read the README? You should have set
+    // functions.config().rendertron.server to where ever you deployed
+    // https://github.com/GoogleChrome/rendertron on AppEngine
+    fetch(`${FIREBASE_CONFIG.rendertron.server}/render/${targetUrl}`)
       .then(function(res) {
         return res.text();
       }).then(function(body) {
@@ -74,8 +80,10 @@ app.get('*', (req, res) => {
     // 2. Umm, Justin, why not just fetch() index.html from the domain?
     //
     // Valid things to ask internet peoples
-    // 1. function doesn't know about the public hosting as far as I can tell (docs don't offer opinion/example)
-    // 2. Could fetch and return...but I found copy+paste the index.html PWA shell into file returns faster
+    // 1. function doesn't know about the public hosting as far as I can tell
+    //    (docs don't offer opinion/example)
+    // 2. Could fetch and return...but I found copy+paste the index.html PWA
+    //    shell into file returns faster
     res.send(pwaShell());
   }
 });
